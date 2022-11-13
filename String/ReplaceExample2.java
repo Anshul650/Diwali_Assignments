d1ee8e4a55b05119fa3ac5a94f302debb3764d56
Anshul Singh <113436978+Anshul650@users.noreply.github.com>
2022-11-13 23:55:58 +0530

    Add files via upload

---
+++ String/ReplaceExample2.java
@@ -0,0 +1,10 @@
+
+class ReplaceExample2 
+{
+	public static void main(String args[]) 
+	{
+        String s1 = "my name is khan my name is java";
+        String replaceString = s1.replace(" ", "%20");
+        System.out.println(replaceString);
+    }
+}
---
+++ String/run.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class run 
+{
+	public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string to be converted to int");
+        String str = sc.nextLine();
+        int new_str = Integer.parseInt(str);
+        System.out.println("after conversion the result is : ");
+        System.out.println(new_str);
+    }
+}
